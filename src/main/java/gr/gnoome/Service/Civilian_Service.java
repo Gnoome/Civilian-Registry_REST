561a7876240cdf386fb25b042e5c1abc9bb03ae0
gnoome <genannos@gmail.com>
2026-09-04 14:28:19 +0300

    1st complete version with all the fuctions of C_Servise & db_manager

--- src/main/java/gr/gnoome/Service/Civilian_Service.java
+++ src/main/java/gr/gnoome/Service/Civilian_Service.java
@@ -1,26 +1,49 @@
 package gr.gnoome.Service;
 
+import java.net.URI;
+
+import java.util.List;
+
+import jakarta.ws.rs.ServiceUnavailableException;
+
 import gr.gnoome.Domain.Person;
 import gr.gnoome.Utility.Database_Manager;
-
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
-import java.net.URI;
-import java.util.*;
 
 @Path("/Civilians")
 public class Civilian_Service {
 
+    private static void CheckDatabaseAvailability(){
+         if(!Database_Manager.existsDatabase()){
+
+           throw new ServiceUnavailableException(Response.status(Response.Status.SERVICE_UNAVAILABLE)
+        .entity("SQL database is unavailable")
+        .type(MediaType.TEXT_PLAIN)
+        .build());
+
+        }
+    }
+
     @POST
     @Consumes( MediaType.APPLICATION_JSON )
     public Response addperson(@Context UriInfo uriInfo, Person person) {
+       
 
-        if(!Database_Manager.existsDatabase()){
-            Database_Manager.createDatabase();
-        }
+       CheckDatabaseAvailability();
 
         if (person == null) {
             throw new BadRequestException("Civilian is empty");
@@ -38,7 +61,7 @@ public class Civilian_Service {
         if (!person.getBirthdate().matches("\\d{2}-\\d{2}-\\d{4}")) {
             throw new BadRequestException("Birthdate is in wrong format");
         }
-        if (!person.getTax().matches("\\d{9}") && person.getTax() != null) {
+        if (person.getTax() != null && !person.getTax().matches("\\d{9}")) {
             throw new BadRequestException("Tax is in wrong format");
         }
 
@@ -49,28 +72,77 @@ public class Civilian_Service {
             Database_Manager.addperson(person);
         }
 
-        // return Response.created(new
-        // URI(uriInfo.getPath()+"/"+person.getId())).build();
 
         URI location = uriInfo.getAbsolutePathBuilder()
                 .path(person.getId())
                 .build();
 
-        return Response.created(location).build();
+        return Response.created(location).entity(person.getId()).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Person> getAllCivilians(){
+
+        CheckDatabaseAvailability();
+        return Database_Manager.viewallpersons();
     }
 
     @GET
     @Path("/search")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Person> getAllCivilians(){
+    public List<Person> getSelectedCivilians(
+      @QueryParam("id") String id,
+      @QueryParam("name") String name,
+      @QueryParam("surname") String surname,
+      @QueryParam("birthdate") String birthdate,
+      @QueryParam("gender") String gender,
+      @QueryParam("address") String address,
+      @QueryParam("tax") String tax){
 
-        return Database_Manager.viewallpersons();
-    }
+        CheckDatabaseAvailability();
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Person> getSelectedCivilians(Person person){
+        Person person = new Person();
+
+        person.setid(id);
+        person.setName(name);
+        person.setSurname(surname);
+        person.setGender(gender);
+        person.setBirthdate(birthdate);
+        person.setAddress(address);
+        person.setTax(tax);
 
         return Database_Manager.ViewSelectedCivilians(person);
     }
+
+   @DELETE
+    @Path("/{id}")
+   public Response DeleteCivilian(@PathParam("id") String id){
+
+        CheckDatabaseAvailability();
+       
+        if(Database_Manager.deleteperson(id)){
+             return Response.noContent().build();
+        }
+        else{
+            throw new NotFoundException();
+        }
+
+   }
+
+   @PATCH
+   @Path("/{id}")
+   public Response UpdateCivilian(@PathParam("id") String id, @QueryParam("address") String address,@QueryParam("tax") String tax){
+
+        CheckDatabaseAvailability();
+
+        if(Database_Manager.updateperson(id,address,tax)){
+
+            return Response.ok().build();
+        }
+        else{
+            throw new BadRequestException();
+        }
+   }
 }
+
